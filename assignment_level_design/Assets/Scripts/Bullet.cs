f8f558ac5f08871021fac522d1539579132b1b22
Andrew Garr <chico2995@gmail.com>
2020-05-27 12:14:42 +0800

    Version1 Game
    This is the first version of the game

---
+++ assignment_level_design/Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    public int damage = 1;
+    public float lifeTime = 0.5f;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyMovement enemy = collision.GetComponent<EnemyMovement>();
+        if (enemy != null)
+        {
+            enemy.GetHit(damage);
+            var effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            Destroy(effect.gameObject, 0.5f);
+        }
+    }
+}
